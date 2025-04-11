cdda43c29d40ec3d8cff5d30f4f857204b43343c
taliabenheim <talia_benheim@brown.edu>
2025-04-11 17:50:38 -0400

    Merge provider info

---
+++ code/02_read_merge_nhc.R
@@ -0,0 +1,45 @@
+library(tidyverse)
+library(dplyr)
+library(stringr)
+
+# Read NHC data from CSV
+df_prov <- read.csv("data/raw/nh_providerinfo_nov2024.csv")
+
+df_prov <- df_prov %>%
+  select(
+    PROVNUM = CMS.Certification.Number..CCN.,
+    fac_name = Provider.Name,
+    ownership = Ownership.Type,
+    n_cert_beds = Number.of.Certified.Beds,
+    qm_rating = QM.Rating,
+    overall_rating = Overall.Rating,
+    health_inspection_rating = Health.Inspection.Rating,
+    staffing_rating = Staffing.Rating,
+    longstay_qm_rating = Long.Stay.QM.Rating,
+    nursing_staff_turnover = Total.nursing.staff.turnover,
+    rn_turnover = Registered.Nurse.turnover,
+    na_hprd = Reported.Nurse.Aide.Staffing.Hours.per.Resident.per.Day,
+    lpn_hprd = Reported.LPN.Staffing.Hours.per.Resident.per.Day,
+    rn_hprd = Reported.RN.Staffing.Hours.per.Resident.per.Day
+  ) %>%
+  mutate(
+    PROVNUM = as.character(PROVNUM),
+    PROVNUM = str_pad(PROVNUM, width = 6, side = "left", pad = "0")
+  )
+
+# Collapse aggregated PBJ to facility level (one row per facility)
+df_pbj <- read.csv("data/intermediate/monthly_pbj")
+agg_pbj <- df_pbj %>%
+    group_by(PROVNUM) %>%
+    summarize(
+        avg_share_hrs_all = mean(share_hrs_all, na.rm = TRUE),
+        avg_share_hrs_rn = mean(share_hrs_rn, na.rm = TRUE),
+        avg_share_hrs_lpn = mean(share_hrs_lpn, na.rm = TRUE),
+        avg_share_hrs_cna = mean(share_hrs_cna, na.rm = TRUE)
+    )
+
+# Merge NHC to PBJ 1:1 on provider ID
+merged_nhc_pbj <- merge(agg_pbj, df_prov, by = "PROVNUM", all.y = TRUE)
+
+# Save CSV
+write.csv(merged_nhc_pbj, "data/intermediate/merged_nhc_pbj", row.names = FALSE)
